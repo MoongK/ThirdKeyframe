94675142ed0992990352d0b38b860e87c0a9d0bb
MoongK <dijiket@naver.com>
2018-03-11 01:39:27 +0900

    keep working

---
+++ Assets/HeadMove.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeadMove : MonoBehaviour {
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+        float x = Input.GetAxisRaw("Mouse X");
+        float y = Input.GetAxisRaw("Mouse Y");
+
+        float maxrotation = 45f;
+        float minrotation = -45f;
+        Vector3 currentrotation = transform.localRotation.eulerAngles;
+        currentrotation.x = Mathf.Clamp(currentrotation.x, minrotation, maxrotation);
+
+
+        //transform.Find("Head").transform.localRotation = Quaternion.Euler(currentrotation);
+
+        transform.Rotate(Vector3.up * x * 50f * Time.deltaTime);
+	}
+}

89dce0f8d2024abdeac96b2cdfbb4debbf9ecaed
MoongK <dijiket@naver.com>
2018-03-12 08:35:08 +0900

    Third

--- Assets/HeadMove.cs
+++ Assets/HeadMove.cs
@@ -4,22 +4,36 @@ using UnityEngine;
 
 public class HeadMove : MonoBehaviour {
 
+
+    float rotationyy, rotationxx;
+    float turnSpeed;
+    
+
 	void Start () {
-		
+        rotationyy = 0f;
+        rotationxx = 0f;
+        turnSpeed = 20f;
 	}
 	
 	void Update () {
-        float x = Input.GetAxisRaw("Mouse X");
-        float y = Input.GetAxisRaw("Mouse Y");
+        float xx = Input.GetAxisRaw("Mouse X");
+        float yy = Input.GetAxisRaw("Mouse Y");
 
-        float maxrotation = 45f;
-        float minrotation = -45f;
-        Vector3 currentrotation = transform.localRotation.eulerAngles;
-        currentrotation.x = Mathf.Clamp(currentrotation.x, minrotation, maxrotation);
+        rotationyy += yy;
+        rotationxx += xx * Time.deltaTime;
+        float finalrotateyy = Mathf.Clamp(rotationyy, -30f, 45f);
+        float finalrotatexx = Mathf.Clamp(rotationxx, -20f, 20f);
 
+        //if (xx == 0f)
+        //    finalrotatexx = 0f;
 
-        //transform.Find("Head").transform.localRotation = Quaternion.Euler(currentrotation);
+        //transform.localRotation = Quaternion.Euler(-finalrotateyy, finalrotatexx, 0f);
+        transform.localRotation = Quaternion.Lerp(transform.localRotation, Quaternion.Euler(-finalrotateyy, finalrotatexx, 0f), turnSpeed * Time.deltaTime);
 
-        transform.Rotate(Vector3.up * x * 50f * Time.deltaTime);
-	}
+        if (Input.GetKey(KeyCode.LeftControl))
+        {
+            print("Crouch!");
+        }
+
+    }
 }
